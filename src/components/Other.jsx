4ecfe60fc755e481a2a3086134fd24d9296746b9
DungHt <dunght97nd@gmail.com>
2024-11-09 01:31:33 +0700

    Add Other.jsx

---
+++ src/components/Other.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+const Other = () => {
+  return (
+    <section className="section bg-[#161617]">
+      <div className="viewport-content">
+        <div className="mb-12 w-full">
+          <h2 className="text-4xl lg:text-5xl font-semibold text-gray">
+            iPhone
+          </h2>
+        </div>
+
+        <div className="flex lg:flex-row flex-col lg:gap-24 gap-9">
+          <ul className="flex flex-col gap-2">
+            <li>
+              <h3 className="text-xs text-gray mb-2">Explore iPhone</h3>
+            </li>
+            <li>
+              <a href="" className="text-xl lg:text-2xl font-semibold">
+                Explore All iPhone
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-xl lg:text-2xl font-semibold">
+                iPhone 15 Pro
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-xl lg:text-2xl font-semibold">
+                iPhone 15
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-xl lg:text-2xl font-semibold">
+                iPhone 14
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-xl lg:text-2xl font-semibold">
+                iPhone 13
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-xl lg:text-2xl font-semibold">
+                iPhone SE
+              </a>
+            </li>
+            <li className="mt-4">
+              <a href="" className="text-sm font-semibold">
+                Compare iPhone
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Switch from Android
+              </a>
+            </li>
+          </ul>
+
+          <ul className="flex flex-col gap-2">
+            <li>
+              <h3 className="text-xs text-gray mb-2">Shop iPhone</h3>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Shop iPhone
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                iPhone Accessories
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Apple Trade In
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Carrier Deals at Apple
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Financing
+              </a>
+            </li>
+          </ul>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <h3 className="text-xs text-gray mb-2">Go Further</h3>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                iPhone Support
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                AppleCare+ for iPhone
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                OS 18 Preview
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Apple Intelligence
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Apps by Apple
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                iPhone Privacy
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                iCloud+
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Wallet, Pay, Card
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-sm font-semibold">
+                Siri
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Other;
